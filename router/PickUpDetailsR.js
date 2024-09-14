e408cec2c5ddc70c3408b1809ca88b0ba7836cd0
Oluwafolakemi Racheal <folakeatolagbe96@gmail.com>
2024-09-14 13:21:55 +0100

    folake

--- router/PickUpDetailsR.js
+++ router/PickUpDetailsR.js
@@ -1,14 +1,14 @@
-// const express = require('express')
-// const { createWaste, deleteWaste, getAllWaste, getOne, updateWaste } = require('../controller/pickUpDetailsC')
-// const { authenticate, isAdmin } = require('../middleware/auth copy')  
-// const { createWasteValidator } = require('../middleware/validator')
+const express = require('express')
+const { createWaste, deleteWaste, getAllWaste, getOne, updateWaste } = require('../controller/pickUpDetailsC')
+const { authenticate, isAdmin } = require('../middleware/auth copy')  
+const { createWasteValidator } = require('../middleware/validator')
 
-// const router = express.Router()
+const router = express.Router()
 
-// router.post('/create-waste/:id',createWasteValidator,createWaste)
-// router.post('/update-waste',authenticate,updateWaste)
-// router.get('/get-all',authenticate,isAdmin,getAllWaste)
-// router.get('/get-one',authenticate,getOne)
-// router.delete('/delete-waste',isAdmin,deleteWaste)
+router.post('/create-waste/:id',createWasteValidator,createWaste)
+router.post('/update-waste',authenticate,updateWaste)
+router.get('/get-all',authenticate,isAdmin,getAllWaste)
+//router.get('/get-one',authenticate,getOne)
+router.delete('/delete-waste',isAdmin,deleteWaste)
 
-// module.exports = router
+module.exports = router
