7270ac82f14c23a0a322131a4192bc420acd6b35
Oluwafolakemi Racheal <folakeatolagbe96@gmail.com>
2024-09-22 19:59:29 +0100

    folake

--- middleware/validator.js
+++ middleware/validator.js
@@ -13,11 +13,14 @@ const Joi = require("joi");
           "string.pattern.base": "Please enter a valid email address.",
         }),
     
-        Password: Joi.string().trim().min(6).required().messages({
-            "string.min": "Password must be at least 6 characters long.",
-            "string.empty": "Password cannot be empty.",
-            "any.required": "Password is required.",
-          }),
+        Password: Joi.string().pattern(new RegExp("^(?=.[!@#$%^&])(?=.*[A-Z]).{7,}$"))
+        .required()
+        .messages({
+            "any.required": "Please provide a password.",
+            "string.empty": "Password cannot be left empty.",
+            "string.pattern.base":
+                "Password must be at least 7 characters long and include at least one uppercase letter and one special character (!@#$%^&*).",
+            }),
     })
     
 const validateLogin = (req, res, next) => {
